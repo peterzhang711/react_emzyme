71fa6b0f88dd346294adca7adad82cbfcb936578
raben4@hotmail.com <raben4@hotmail.com>
2020-08-14 12:39:46 +1000

    add reudx to app

---
+++ src/createStore.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware } from 'redux'
+import ReduxThunk from 'redux-thunk'
+import RootReducer from './store'
+
+
+export const middlewares = [ReduxThunk]
+
+export const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore)
+
+export const store = createStoreWithMiddleware(RootReducer)
---
+++ src/store/index.js
@@ -0,0 +1,6 @@
+import { combineReducers} from 'redux'
+import successe from './successReducer'
+
+export default combineReducers ({
+    successe
+})
